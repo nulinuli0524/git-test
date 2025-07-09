b1e463ac23206e44456540aa39f783578485d9ce
nulinuli0524 <105159045+nulinuli0524@users.noreply.github.com>
2025-07-09 15:53:08 +0800

    Create pdf2pclm.cpp

---
+++ harmony-module/cpp/pdf2pclm.cpp
@@ -0,0 +1,6 @@
+#include <cstdio>
+
+extern "C" void convertPdfToPcl(const char* input, const char* output) {
+    // stub: just print paths
+    std::printf("Converting %s -> %s\n", input, output);
+}
