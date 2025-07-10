aa3411b9a73e94b738d52f33a5a03bbb2d50ff55
nulinuli0524 <105159045+nulinuli0524@users.noreply.github.com>
2025-07-10 18:16:12 +0800

    Update gs_napi.cpp

--- native/gs_napi.cpp
+++ native/gs_napi.cpp
@@ -1,49 +1,27 @@
-#include <napi/native_api.h>
 #include "iapi.h"
+#include <cstdio>
+#include <cstdlib>
 
-static napi_value Convert(napi_env env, napi_callback_info info) {
-  size_t argc = 2;
-  napi_value argv[2];
-  napi_get_cb_info(env, info, &argc, argv, nullptr, nullptr);
+extern "C" int convert(const char* pdf_path, const char* pclm_path) {
+    void* gs = nullptr;
+    if (gsapi_new_instance(&gs, nullptr) < 0) {
+        return -1;
+    }
 
-  char input[256], output[256];
-  size_t len;
-  napi_get_value_string_utf8(env, argv[0], input, sizeof(input), &len);
-  napi_get_value_string_utf8(env, argv[1], output, sizeof(output), &len);
+    char out_arg[512];
+    std::snprintf(out_arg, sizeof(out_arg), "-sOutputFile=%s", pclm_path);
 
-  void* gs = nullptr;
-  gsapi_new_instance(&gs, nullptr);
-
-  const char* args[] = {
-    "gs", "-dNOPAUSE", "-dBATCH",
-    "-sDEVICE=pclm",
-    "-sOutputFile", output,
-    input
-  };
-
-  int code = gsapi_init_with_args(gs, 7, const_cast<char**>(args));
-  gsapi_exit(gs);
-  gsapi_delete_instance(gs);
-
-  napi_value result;
-  napi_create_int32(env, code, &result);
-  return result;
-}
-
-static napi_module gsModule = {
-  .nm_version = 1,
-  .nm_register_func = [](napi_env env, napi_value exports) {
-    napi_property_descriptor desc = {
-      "convert", nullptr, Convert, nullptr, nullptr, nullptr, napi_default, nullptr
+    const char* gs_args[] = {
+        "gs",
+        "-dNOPAUSE",
+        "-dBATCH",
+        "-sDEVICE=pclm",
+        out_arg,
+        pdf_path
     };
-    napi_define_properties(env, exports, 1, &desc);
-    return exports;
-  },
-  .nm_modname = "gs_napi",
-  .reserved = {0}
-};
 
-__attribute__((constructor))
-static void Register() {
-  napi_module_register(&gsModule);
+    int code = gsapi_init_with_args(gs, 6, const_cast<char**>(gs_args));
+    gsapi_exit(gs);
+    gsapi_delete_instance(gs);
+    return code;
 }
