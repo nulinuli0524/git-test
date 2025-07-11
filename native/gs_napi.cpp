c582a29ce789fd5a7e267cf176a1fe310b159e20
nulinuli0524 <105159045+nulinuli0524@users.noreply.github.com>
2025-07-11 15:38:24 +0800

    Update gs_napi.cpp

--- native/gs_napi.cpp
+++ native/gs_napi.cpp
@@ -3,7 +3,6 @@
 #include <cstdio>
 
 static napi_value JsConvert(napi_env env, napi_callback_info info) {
-    // 1) 拿到入参
     size_t argc = 2;
     napi_value argv[2];
     napi_get_cb_info(env, info, &argc, argv, nullptr, nullptr);
@@ -13,12 +12,12 @@ static napi_value JsConvert(napi_env env, napi_callback_info info) {
     napi_get_value_string_utf8(env, argv[0], pdfPath, sizeof(pdfPath), &len);
     napi_get_value_string_utf8(env, argv[1], pclmPath, sizeof(pclmPath), &len);
 
-    // 2) 调用 Ghostscript
     void* gs = nullptr;
     if (gsapi_new_instance(&gs, nullptr) < 0) {
         napi_value err; napi_create_int32(env, -1, &err);
         return err;
     }
+
     char outArg[512];
     std::snprintf(outArg, sizeof(outArg), "-sOutputFile=%s", pclmPath);
     const char* gs_args[] = {
@@ -28,7 +27,6 @@ static napi_value JsConvert(napi_env env, napi_callback_info info) {
     gsapi_exit(gs);
     gsapi_delete_instance(gs);
 
-    // 3) 返回结果
     napi_value result;
     napi_create_int32(env, code, &result);
     return result;
@@ -44,7 +42,6 @@ static napi_value Init(napi_env env, napi_value exports) {
     return exports;
 }
 
-// 模块名改为纯名字，不带 lib/.so
 static napi_module moduleDef = {
     .nm_version       = 1,
     .nm_register_func = Init,
